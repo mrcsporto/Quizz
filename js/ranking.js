9c543353578e1fbf578db49f3541701d6e0b226d
mrcsporto <mrcsporto@gmail.com>
2023-01-23 20:00:15 -0300

    limit rank to 10 first

--- js/ranking.js
+++ js/ranking.js
@@ -1,15 +1,18 @@
 users = JSON.parse(localStorage.getItem("users"))
 let sortedUsers = users.sort((a, b) => b.score - a.score)
 
-const $tableP = document.getElementById('table');
-$tableP.insertAdjacentHTML('afterbegin', createTable());
+const table = document.getElementById('table');
+table.insertAdjacentHTML('afterbegin', createTable());
 
 function createTableRow(data) {
   const arr = data.map(value => { return `<td>${value}</td>`; });
   return `<tr>${arr.join('')}</tr>`;
 }
+
+
 function createTable() {
-  const tableRows = users.map((value, i) => {
+  let limitedArray = users.slice(0, 10);
+  const tableRows = limitedArray.map((value, i) => {
     return createTableRow([users[i].name, users[i].score]);
   });
 
