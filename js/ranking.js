0fd46b03b5419e9e1f3838ea60f56615fab1513a
mrcsporto <mrcsporto@gmail.com>
2023-01-23 11:33:15 -0300

    add ranking and user register

--- js/ranking.js
+++ js/ranking.js
@@ -1,17 +1,44 @@
 users = JSON.parse(localStorage.getItem("users"))
 let sortedUsers = users.sort((a, b) => b.score - a.score)
 
-const $table = document.getElementById('table');
-$table.insertAdjacentHTML('afterbegin', createTable());
+const $tableP = document.getElementById('tablePro');
+$tableP.insertAdjacentHTML('afterbegin', createTable());
+
+const $tableA = document.getElementById('tableAca');
+$tableA.insertAdjacentHTML('afterbegin', createTableAca());
 
 function createTableRow(data) {
   const arr = data.map(value => { return `<td>${value}</td>`; });
   return `<tr>${arr.join('')}</tr>`;
 }
-
 function createTable() {
   const tableRows = users.map((value, i) => {
-    return createTableRow([users[i].name, users[i].profession, users[i].score]);
+    if (users[i].profession === "profissional"){
+      return createTableRow([users[i].name, users[i].profession, users[i].score]);
+    }
+  });
+
+  return `
+      <table>
+        <thead>
+          <tr>
+            <td>Nome</td>
+            <td>Ocupação</td>
+            <td>Pontos</td>
+          </tr>
+        </thead>
+        <tbody>
+          ${tableRows.join('')}
+        </tbody>
+      </table>
+    `;
+}
+
+function createTableAca() {
+  const tableRows = users.map((value, i) => {
+    if (users[i].profession === "academico"){
+      return createTableRow([users[i].name, users[i].profession, users[i].score]);
+    }
   });
 
   return `
